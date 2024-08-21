4d4240a2a3e9768cbc90163635cd2dee49bc2e8e
Diana <dayanatodorova14@gmail.com>
2024-08-21 15:13:46 +0300

    Adding calendar

---
+++ Controllers/CalendarController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TM2.Data;
+using TM2.ViewModels;
+
+namespace TM2.Controllers
+{
+    public class CalendarController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CalendarController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(int? month, int? year)
+        {
+            
+            var currentDate = DateTime.Now;
+
+            var displayMonth = month ?? currentDate.Month;
+            var displayYear = year ?? currentDate.Year;
+
+            
+            var firstDayOfMonth = new DateTime(displayYear, displayMonth, 1);
+
+            
+            var daysInMonth = DateTime.DaysInMonth(displayYear, displayMonth);
+
+            
+            var tasksForMonth = _context.Tasks
+                .Where(t => t.DueTime.Year == displayYear && t.DueTime.Month == displayMonth)
+                .ToList();
+
+            var viewModel = new CalendarViewModel
+            {
+                DisplayMonth = displayMonth,
+                DisplayYear = displayYear,
+                FirstDayOfMonth = firstDayOfMonth,
+                DaysInMonth = daysInMonth,
+                Tasks = tasksForMonth
+            };
+
+            return View(viewModel);
+        }
+    }
+
+}
---
+++ ViewModels/CalendarViewModel.cs
@@ -0,0 +1,13 @@
+﻿using TM2.Models;
+
+namespace TM2.ViewModels
+{
+    public class CalendarViewModel
+    {
+        public int DisplayMonth { get; set; }
+        public int DisplayYear { get; set; }
+        public DateTime FirstDayOfMonth { get; set; }
+        public int DaysInMonth { get; set; }
+        public List<Tasks> Tasks { get; set; }
+    }
+}
